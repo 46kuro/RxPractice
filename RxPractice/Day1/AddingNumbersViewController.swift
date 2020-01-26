700f0e5f086b1189c28f21b9c2c75221df2c1d9a
shinji46kuro <shinji.46.kuro@gmail.com>
2020-01-26 22:32:07 +0900

    Implement AddingNumbers
    ## Questions
    - row29: closure is map operator?
    - This example(CombineLatest) is no subscribe method. Is it hot observable?(How to distinguish? CombineLatest?Bind?)

--- RxPractice/Day1/AddingNumbersViewController.swift
+++ RxPractice/Day1/AddingNumbersViewController.swift
@@ -6,13 +6,31 @@
 //  Copyright © 2020 Shinji Kurosawa. All rights reserved.
 //
 
+import RxCocoa
+import RxSwift
 import UIKit
 
 class AddingNumbersViewController: UIViewController {
 
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!    
+    
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Adding numbers"
+        
+        Observable.combineLatest(textField1.rx.text.orEmpty, 
+                                 textField2.rx.text.orEmpty,
+                                 textField3.rx.text.orEmpty) { value1, value2, value3 -> Int in
+                                    return (Int(value1) ?? 0) + (Int(value2) ?? 0) + (Int(value3) ?? 0)
+        }
+        .map { $0.description }
+        .bind(to: resultLabel.rx.text)
+        .disposed(by: disposeBag)
     }
 }
